517b55a2037725427b9fa3f06bfd89bf53632c04
Arne Olai Molland <arneolaim@gmail.com>
2018-11-12 20:06:40 +0100

    :sparkles: Finished implementation of BFS

--- src/main/java/no/hvl/dat108/Graph.java
+++ src/main/java/no/hvl/dat108/Graph.java
@@ -1,5 +1,9 @@
 package no.hvl.dat108;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.PriorityQueue;
 
 class Graph {
@@ -9,11 +13,80 @@ class Graph {
         vertexes = new PriorityQueue<Vertex>(6, new Vertex());
     }
 
+    public Graph(PriorityQueue<Vertex> vertexes) {
+        this.vertexes = vertexes;
+    }
+
+
     public Vertex maxDegreeVertex() {
         return vertexes.peek();
     }
 
+    void breadthFirstSearch(Vertex v) {
+         ArrayList<Vertex> visited = new ArrayList<Vertex>();
+
+        LinkedList<Vertex> queue = new LinkedList<Vertex>();
+
+        visited.add(v);
+        queue.add(v);
+
+        while(queue.size() != 0) {
+            v = queue.poll();
+            System.out.println(v.getName() +" ");
+
+            for(Vertex n : v.edges) {
+                if(!visited.contains(n))
+                {
+                    visited.add(n);
+                    queue.add(n);
+                }
+            }
+        }
+    }
+
     public void addVertex(Vertex v) {
         vertexes.add(v);
+    }
+
+    public void removeVertex(Vertex v) {
+        vertexes.remove(v);
+    }
+
+    public void addEdge(Vertex v1, Vertex v2) {
+        v1.edges.add(v2);
+        v2.edges.add(v1);
+    }
+
+    public void removeEdge(Vertex v1, Vertex v2) {
+        v1.edges.remove(v2);
+        v2.edges.remove(v1);
+    }
+
+    public void addUniEdge(Vertex fromEdge, Vertex toEdge) {
+        fromEdge.edges.add(toEdge);
+    }
+
+    public void removeUniEdge(Vertex fromEdge, Vertex toEdge) {
+        fromEdge.edges.remove(toEdge);
+    }
+
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append(hasUniEdges() ? "[Unidirectional]\n" : "[Non-unidirectional]\n");
+        builder.append(String.format("Graph contains %d vertexes: \n", vertexes.size()));
+        for(Vertex  v : vertexes) {
+            builder.append(v.toString());
+        }
+
+        return builder.toString();
+    }
+       
+    private boolean hasUniEdges () {
+        for(Vertex v : vertexes) {
+            if(v.hasUniEdges()) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/main/java/no/hvl/dat108/Vertex.java
+++ src/main/java/no/hvl/dat108/Vertex.java
@@ -4,12 +4,50 @@ import java.util.HashSet;
 
 public class Vertex implements Comparator<Vertex> {
     HashSet<Vertex> edges;
-
+    private String name;
+    
     public Vertex() {
+        edges = new HashSet<Vertex>();
+    }
+
+    public Vertex(String name) {
+        this.name = name;
         edges = new HashSet<Vertex>();
     }
 
     public int compare(Vertex v1, Vertex v2) {
         return Integer.compare(v1.edges.size(), v2.edges.size());
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append(String.format("Vertex %s with %d edges%s", this.name, edges.size(), (edges.size()==0) ? "\n" : ":\n"));
+        for(Vertex v : edges) {
+            if(!v.edges.contains(this)) {
+                builder.append(String.format("Unidirectional from %s to %s \n", this.name, v.name));
+            }
+            else {
+            builder.append(String.format("From %s to %s \n", this.name, v.name));
+            }
+        }
+        return builder.toString();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean hasUniEdges() {
+        for(Vertex v : edges) {
+            if(this.edges.contains(v) && v.edges.contains(this)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
